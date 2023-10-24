2983a1d78615f3089c56bf244ff94e69e67ea7a7
tanzim10 <tanzimfarhan@gmail.com>
2023-10-24 23:00:01 +0600

    Changing the django settings for applications and database

--- stockify/stockify/settings.py
+++ stockify/stockify/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'cirspy_bootstrap4',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,11 @@ WSGI_APPLICATION = 'stockify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'stocks',
+        'USER':'tanzim',
+        'PASSWORD':'Tfra1024',
+        'PORT':'5432',
     }
 }
 
@@ -121,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'admin'
+LOGIN_URL = 'login'
+
+
