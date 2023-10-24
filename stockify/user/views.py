bbedecb16642b682187b32b0f423e0e3d4c6b9ef
tanzim10 <tanzimfarhan@gmail.com>
2023-10-25 00:08:18 +0600

    Modifing models for users

--- stockify/user/views.py
+++ stockify/user/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm,ProfileForm
+from django.contrib import messages
 
 # Create your views here.
