bf6b003694053bfc37237548856b8d57529c0fca
tanzim10 <tanzimfarhan@gmail.com>
2023-10-26 01:12:20 +0600

    Settings issue resolve with new app stock

--- stockify/stockify/settings.py
+++ stockify/stockify/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'cirspy_bootstrap4',
     'corsheaders',
     'user.apps.UserConfig'
 ]
@@ -51,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'stockify.urls'
