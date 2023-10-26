8fbcc83d441bb7a4bd3d2d7d7a185e04042f423e
tanzim10 <tanzimfarhan@gmail.com>
2023-10-26 16:11:28 +0600

    Stock app with models and intial view

--- stockify/stock/views.py
+++ stockify/stock/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse
+import pandas as pd
+from . models import StockDeposit,StockList
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from . import utils
+from django.db import transaction
+import numpy as np
+from django.http import JsonResponse
 
 # Create your views here.
