95c794591e223c30fa1778a1ddcc14699b50f423
tanzim10 <tanzimfarhan@gmail.com>
2023-10-29 16:50:57 +0600

    Utils in stock updated and frontend

--- stockify/stock/utils.py
+++ stockify/stock/utils.py
@@ -193,6 +193,173 @@ def get_predictions(symbol,date_end):
     pred_price = scaler.inverse_transform(pred_price)
 
 
+def get_dates(end_dates):
+    """
+    Generate a list of years between a fixed start date and a specified end date.
+
+    This function returns a list of unique years that fall between January 1, 2012, 
+    and the provided `end_dates`. The primary purpose is to identify the range of years 
+    covered by a dataset with the aforementioned constraints.
+
+    Parameters:
+    -----------
+    end_dates : str
+        The end date as a string in the format "YYYY-MM-DD".
+
+    Returns:
+    --------
+    List[int]
+        A list of unique years between January 1, 2012, and the provided `end_dates`.
+
+    Example:
+    --------
+    >>> get_dates("2015-06-30")
+    [2012, 2013, 2014, 2015]
+
+    Notes:
+    ------
+    The function uses the date format "%Y-%m-%d" and begins its range from January 1, 2012.
+    """
+    date_format = "%Y-%m-%d"
+    datetime_obj = datetime.strptime(end_dates, date_format)
+    start_date = date(2012, 1, 1)
+    end_date = datetime_obj.date()
+
+    current_date = start_date
+    years = []
+
+    while current_date <= end_date:
+        years.append(current_date.year)
+        current_date += timedelta(days=1)
+
+    return years
+
+
+def get_graph():
+    """
+    Capture the current Matplotlib plot as a base64-encoded PNG.
+
+    This function captures the active Matplotlib figure, encodes it as a PNG, and 
+    then converts the PNG bytes into a base64 encoded string. This base64 string 
+    can be used directly for embedding the image into web pages.
+
+    Returns:
+    --------
+    str
+        A base64-encoded PNG representation of the current Matplotlib plot.
+
+    Example:
+    --------
+    >>> plt.plot([1, 2, 3], [1, 2, 3])
+    >>> base64_str = get_graph()
+
+    Notes:
+    ------
+    Ensure a plot is active (i.e., has been created) before calling this function.
+    The function uses the `plt.savefig` method from Matplotlib to capture the plot.
+    """
+
+    buffer = BytesIO()
+    plt.savefig(buffer, format ='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+
+    return graph
+
+def get_plot(valid, train):
+    """
+    Generate a plot visualizing training data, validation data, and predictions.
+
+    This function uses Matplotlib to visualize the `Close` prices from both the 
+    training and validation datasets, along with the predictions for the validation set.
+    The resulting plot is then captured as a base64 encoded string for easy embedding 
+    into web pages or other outputs.
+
+    Parameters:
+    -----------
+    valid : pandas.DataFrame
+        The validation dataset containing columns 'Close' and 'Predictions'.
+    train : pandas.DataFrame
+        The training dataset containing a 'Close' column.
+
+    Returns:
+    --------
+    str
+        A base64-encoded PNG representation of the generated plot.
+
+    Example:
+    --------
+    >>> train_data = pd.DataFrame({'Close': [1, 2, 3, 4, 5]})
+    >>> valid_data = pd.DataFrame({'Close': [5, 6, 7], 'Predictions': [5.1, 6.1, 7.1]})
+    >>> base64_str = get_plot(valid_data, train_data)
+    >>> print(base64_str[:50])  # Displaying the first 50 characters of the encoded string
+    'iVBORw0KG...'
+
+    Notes:
+    ------
+    The function relies on a helper function `get_graph` to capture the plot as a 
+    base64 string.
+    """
+
+    plt.switch_backend('AGG')
+    plt.figure(figsize=(15,6))
+    plt.title('Model')
+    plt.xlabel('Date', fontsize = 18)
+    plt.ylabel('Close Price USD $')
+    plt.plot(train['Close'])
+    plt.plot(valid[['Close','Predictions']])
+    plt.xticks(rotation=45)
+    plt.legend(['Train', 'Val', 'Predictions'], loc='upper right')
+    plt.tight_layout()
+    graph = get_graph()
+
+    return graph
+
+
+def pred_date(end_date):
+    """
+    Calculate the next date given an input date.
+
+    Given an input date in the format "YYYY-MM-DD", this function returns the 
+    next date as a string in the same format.
+
+    Parameters:
+    -----------
+    end_date : str
+        A date string in the format "YYYY-MM-DD".
+
+    Returns:
+    --------
+    str
+        The next date in the format "YYYY-MM-DD".
+
+    Example:
+    --------
+    >>> end_date = "2023-05-01"
+    >>> next_date = pred_date(end_date)
+    >>> print(next_date)
+    '2023-05-02'
+
+    Notes:
+    ------
+    The function utilizes the datetime module to perform the date manipulations.
+    """
+
+    date_format = "%Y-%m-%d"
+    date_obj = datetime.strptime(end_date, date_format)
+    next_date = date_obj + timedelta(days=1)
+    next_date_string = next_date.strftime(date_format)
+
+    return next_date_string
+
+
+
+
+
+
 
 
 
